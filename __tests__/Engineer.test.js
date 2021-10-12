a00b3e6debf2e032247f4d78bc175603d35362b5
Cabral Williams <cabral.williams@gmail.com>
2021-10-12 11:31:24 -0500

    failing engineer method tests added

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -10,3 +10,15 @@ test('creates an Engineer object', () => {
     expect(engineer.email).toEqual("cabral.williams@gmail.com");
     expect(engineer.github).toEqual("cabralwilliams");
 });
+
+test('returns Engineer as role of the employee', () => {
+    const engineer = new Engineer("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com","cabralwilliams");
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
+
+test('return GitHub url of the employee', () => {
+    const engineer = new Engineer("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com","cabralwilliams");
+
+    expect(engineer.getGithub()).toEqual(`https://github.com/${engineer.github}`);
+});
