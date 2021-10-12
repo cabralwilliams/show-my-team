579b749c5129c3b8c1dfd1bcf406759713b24413
Cabral Williams <cabral.williams@gmail.com>
2021-10-12 10:26:38 -0500

    Employee class created to pass first test - employeeId property adjusted to id in Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,6 +5,6 @@ test("creates an Employee object", () => {
     const employee = new Employee("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com");
     
     expect(employee.name).toBe("Cabral");
-    expect(employee.employeeId).toEqual(expect.any(Number));
+    expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe("cabral.williams@gmail.com");
 });
