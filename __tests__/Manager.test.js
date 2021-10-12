93359aa9ccb6dd6cc1f7d799c89855426acafb6c
Cabral Williams <cabral.williams@gmail.com>
2021-10-12 11:01:06 -0500

    manager object passing all test finished

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -10,3 +10,9 @@ test("creates a manager object", () => {
     expect(manager.email).toEqual("cabral.williams@gmail.com");
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
+
+test("returns Manager as the manager's role", () => {
+    const manager = new Manager("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com",Math.ceil(Math.random()*100));
+
+    expect(manager.getRole()).toEqual("Manager");
+});
