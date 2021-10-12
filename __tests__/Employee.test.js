2dac27592fedb1ebba47313b6905f709593d9092
Cabral Williams <cabral.williams@gmail.com>
2021-10-12 10:34:31 -0500

    failing tests created for Employee class methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,27 @@ test("creates an Employee object", () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe("cabral.williams@gmail.com");
 });
+
+test("return the name of the employee", () => {
+    const employee = new Employee("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com");
+
+    expect(employee.getName()).toEqual("Cabral");
+});
+
+test("return the employee id", () => {
+    const employee = new Employee("Cabral",525600,"cabral.williams@gmail.com");
+
+    expect(employee.getId()).toEqual(525600);
+});
+
+test("return the employee email address", () => {
+    const employee = new Employee("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com");
+
+    expect(employee.getEmail()).toEqual("cabral.williams@gmail.com");
+});
+
+test("return the employee's role", () => {
+    const employee = new Employee("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com");
+
+    expect(employee.getRole()).toEqual("Employee");
+});
