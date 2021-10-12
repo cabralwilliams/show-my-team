704f0fe1656217d99c58f4e60ba3b6cbcd07dfeb
Cabral Williams <cabral.williams@gmail.com>
2021-10-12 12:10:28 -0500

    created failing method tests for Intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -10,3 +10,15 @@ test('creates an Intern object', () => {
     expect(intern.email).toEqual("cabral.williams@gmail.com");
     expect(intern.school).toEqual("Rice University");
 });
+
+test('returns Intern as role of the employee', () => {
+    const intern = new Intern("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com","Rice University");
+
+    expect(intern.getRole()).toEqual("Intern");
+});
+
+test('returns the school that the intern attends', () => {
+    const intern = new Intern("Cabral",Math.floor(Math.random()*525601),"cabral.williams@gmail.com","Rice University");
+
+    expect(intern.getSchool()).toEqual(intern.school);
+});
